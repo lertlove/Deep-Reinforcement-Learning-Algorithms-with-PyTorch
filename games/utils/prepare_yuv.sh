5108130ec0cf949c49d377d0cca7f2464011ae66
Lert Leela <lertluck.l@obodroid.com>
2022-05-21 09:49:33 +0700

    prepare yuv

--- games/utils/prepare_yuv.sh
+++ games/utils/prepare_yuv.sh
@@ -1,18 +1,32 @@
-# @echo off
-# set back=%cd%
+### Resolution
+RESOLUTION="416x240"
+RATE=50
+INTERVAL=$((1000/$RATE)) #rate=50 -> interval = 1000/rate
+
 ROOT_SOURCE="/mnt/nas/openImageNet/metadata/list_files/split/"
 
-TARGET_DIR="/mnt/nas/openImageNet/metadata/list_files/yuv/"
+TARGET_DIR="/mnt/nas/openImageNet/metadata/list_files/yuv/${RESOLUTION}_${RATE}/"
 
+mkdir $TARGET_DIR
+
+NUM_DIR=`ls $ROOT_SOURCE |wc -l`
 cd $ROOT_SOURCE
-for d in */ ; do
-    d=$(echo $d | sed 's:/*$::')
-    echo "$d"
-    ffmpeg -i $d/%06d.jpg -s 1024x640 -pix_fmt yuv420p -r 200 "$TARGET_DIR$d.yuv"
+
+for ((d=11;d<$NUM_DIR;d++)); 
+do 
+   # your-unix-command-here
+   echo $d
+   ffmpeg -i $d/%06d.jpg -s $RESOLUTION -pix_fmt yuv420p -r $INTERVAL "$TARGET_DIR$d.yuv"
+   wait
 done
 
 
-
+# printf '%s\0' */ | sort -zV | while read -rd '' d; do 
+#     d=$(echo $d | sed 's:/*$::')
+#     echo "$d"
+    # ffmpeg -i $d/%06d.jpg -s $RESOLUTION -pix_fmt yuv420p -r $INTERVAL "$TARGET_DIR$d.yuv"
+    # wait
+# done
 
 # ffmpeg -i dataset/%03d.jpg -s 1024x640 -pix_fmt yuv420p -r 200 f.yuv
 # ffplay -f rawvideo -pixel_format yuv420p -s 1024x640 -i f.yuv 
